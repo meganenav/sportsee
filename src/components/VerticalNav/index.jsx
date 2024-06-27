173cfbb7645283b315d2c6caaa29afada5184aed
Mégane Navarro <mnavarro@live.fr>
2024-06-27 15:41:56 +0200

    Mise en place de la structure de la page

---
+++ src/components/VerticalNav/index.jsx
@@ -0,0 +1,18 @@
+import sitIcon from '../../images/sit.svg'
+import swimIcon from '../../images/swim.svg'
+import bikeIcon from '../../images/bike.svg'
+import gymIcon from '../../images/gym.svg'
+
+export default function Home() {
+    return (
+        <div className="secondary-nav">
+            <nav>
+                <img src={sitIcon} alt="Icône menu yoga"></img>
+                <img src={swimIcon} alt="Icône menu natation"></img>
+                <img src={bikeIcon} alt="Icône menu vélo"></img>
+                <img src={gymIcon} alt="Icône menu musculation"></img>
+            </nav>
+            <p>Copiryght, SportSee 2020</p>
+        </div>
+    )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import '../../sass/main.scss'
+import HorizontalNav from '../../components/HorizontalNav/'
+import VerticalNav from '../../components/VerticalNav/'
+
+export default function Home() {
+    return (
+        <>
+            <HorizontalNav />
+            <VerticalNav />
+        </>
+    )
+}
